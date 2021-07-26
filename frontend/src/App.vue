e7b048e4ee3fb6b3770ed1d88e792c5d14b7b5fd
Sinem Nurioğlu <65951748+sinemn@users.noreply.github.com>
2021-07-26 18:59:39 -0400

    content wrapper created

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,30 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+
+  <div class="container">
+    <router-view></router-view>
   </div>
+    
 </template>
 
+<script>
+
+export default {
+
+}
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+
+body {
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+  font-family: 'Open Sans', sans-serif;
+  background-color: #3B3B45;
 }
 
-#nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+
 </style>

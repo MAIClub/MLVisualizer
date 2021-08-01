7131d77ad7cd0f8850ebdae7f3a834622d12d8a4
Sinem Nurioğlu <65951748+sinemn@users.noreply.github.com>
2021-08-01 12:54:49 -0400

    unrelated classes deleted

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -12,30 +12,4 @@
 
 <script></script>
 
-<style lang="scss">
-.main {
-  margin-top: 3em;
-  color: white;
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-}
-
-.container {
-  max-width: 1000px;
-  margin: 3em;
-  &__top {
-    display: flex;
-    justify-content: center;
-    height: 100%;
-    &__menu {
-      margin-right: 5em;
-    }
-  }
-  &__bottom {
-    display: flex;
-    justify-content: center;
-    margin-top: 0px;
-  }
-}
-</style>
+<style lang="scss"></style>
